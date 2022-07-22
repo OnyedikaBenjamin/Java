e7b71d0f358946fb02190f44fcceea5bc0850d3f
Ben Billion <107368386+Udegbunamonyedikabenjamin@users.noreply.github.com>
2022-07-22 13:52:32 +0100

    practice

---
+++ src/Fourth.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Fourth {
+
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int number;
+
+            System.out.print("enter the number : ");
+            number = input.nextInt();
+            number += 1;
+
+        System.out.println(number);
+        }
+
+
+
+    }
+
+
